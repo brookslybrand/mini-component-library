87fca009165afe8dea59f6533be9ecd5df71eb0e
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2021-03-24 08:08:01 -0500

    Add progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,11 +1,71 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  let Component;
+  if (size === "large") {
+    Component = LargeProgressBarWrapper;
+  } else if (size === "medium") {
+    Component = MediumProgressBarWrapper;
+  } else if (size === "small") {
+    Component = SmallProgressBarWrapper;
+  }
+
+  if (Component === undefined) {
+    throw new Error(`Invalid size of ${size}`);
+  }
+
+  return (
+    <Component
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin={0}
+      aria-valuemax={100}
+    >
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <ProgressWrapper>
+        <Progress style={{ "--width": `${value}%` }} />
+      </ProgressWrapper>
+    </Component>
+  );
 };
+
+const ProgressBarWrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
+
+const ProgressWrapper = styled.div`
+  border-radius: inherit;
+  height: 100%;
+  /* trim corners when progress is near 100% */
+  overflow: hidden;
+`;
+
+const Progress = styled.div`
+  background-color: ${COLORS.primary};
+  width: var(--width);
+  height: 100%;
+  border-radius: inherit;
+  border-top-right-radius: 0px;
+  border-bottom-right-radius: 0px;
+`;
+
+const LargeProgressBarWrapper = styled(ProgressBarWrapper)`
+  height: 24px;
+  border-radius: 8px;
+  padding: 4px;
+`;
+const MediumProgressBarWrapper = styled(ProgressBarWrapper)`
+  height: 12px;
+  border-radius: 4px;
+`;
+const SmallProgressBarWrapper = styled(ProgressBarWrapper)`
+  height: 8px;
+  border-radius: 4px;
+`;
 
 export default ProgressBar;
